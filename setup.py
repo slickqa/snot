71be0a34f4f2b7dada559a8d6d7445ac0826d078
Jason Corbett <jasoncorbett@gmail.com>
2013-10-07 09:47:18 -0600

    updating setup so that you can install snot now

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ from setuptools import setup, find_packages
 setup(
     name="slickqa-snot",
     description="A plugin to nose to allow results from python tests to be put into slick.",
-    version="1.0" + open("build.txt").read(),
+    version="1.0-" + open("build.txt").read().strip(),
     license="License :: OSI Approved :: Apache Software License",
     long_description=open('README.txt').read(),
-    packages=find_packages(exclude=['distribute_setup']),
+    py_modules=['snot'],
+    #packages=find_packages(exclude=['distribute_setup']),
     #package_data={'': ['*.txt', '*.rst', '*.html']},
     #include_package_data=True,
     install_requires=['slickqa>=2.0.34', 'nose'],
