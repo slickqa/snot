f3ea55be43e695572b29bc9a273bba955ebaf3a8
railesax <railesax@adobe.com>
2016-12-07 14:51:00 -0700

    Removing DocStringMetaData so other projects can use it

--- snot.py
+++ snot.py
@@ -1,19 +1,22 @@
-import nose, nose.plugins, nose.case, nose.config
-from slickqa import SlickQA, Testcase, ResultStatus, RunStatus, Step, Result, make_result_updatable, make_testrun_updatable
-from slickqa.connection import SlickConnection
-
-import re
+import datetime
+import importlib
+import itertools
+import logging
 import os
 import sys
-import logging
-import docutils.core
-import datetime
-import traceback
-import itertools
-import importlib
 import time
-from io import StringIO
+import traceback
 from unittest import SkipTest
+
+import nose
+import nose.case
+import nose.config
+import nose.plugins
+
+from slickqa import SlickQA, Testcase, ResultStatus, RunStatus, Step, Result, make_result_updatable, \
+    make_testrun_updatable, DocStringMetaData
+from slickqa.connection import SlickConnection
+
 try:
     from ConfigParser import SafeConfigParser
 except:
@@ -66,61 +69,6 @@ def add_file_to_testrun(path, fileobj=None):
     """
     if testrun is not None:
         testrun.add_file(path, fileobj)
-
-
-class DocStringMetaData(object):
-
-    def __init__(self, func):
-        if hasattr(func, '__doc__') and func.__doc__ is not None:
-            dom = docutils.core.publish_doctree(func.__doc__).asdom()
-            if dom is not None and dom.firstChild is not None and dom.firstChild.nodeName == 'document':
-                document = dom.firstChild
-                if document.hasChildNodes() and document.firstChild.nodeName == 'paragraph':
-                    self.name = document.firstChild.firstChild.nodeValue
-                    if len(document.childNodes) > 1:
-                        for node in document.childNodes[1:]:
-                            self.process_node(node)
-                else:
-                    self.name = self.get_name_from_function_name(func)
-                    for node in document.childNodes:
-                        self.process_node(node)
-        else:
-            self.name = self.get_name_from_function_name(func)
-
-    def get_name_from_function_name(self, func):
-        if hasattr(func, '__name__') and func.__name__ is not None and func.__name__ != "":
-            s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', func.__name__)
-            s2 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-            return re.sub(r'_', ' ', re.sub(r'_?[tT]est$', '', re.sub(r'^[Tt]est_?', '', s2))).capitalize()
-
-    def process_node(self, node):
-        if node.nodeName == 'block_quote':
-            for child_node in node.childNodes:
-                self.process_node(child_node)
-        if node.nodeName == 'field_list':
-            for child_node in node.childNodes:
-                self.process_node(child_node)
-        if node.nodeName == 'paragraph':
-            if hasattr(self, 'purpose'):
-                self.purpose = self.purpose + '\n\n' + node.firstChild.nodeValue
-            else:
-                self.purpose = node.firstChild.nodeValue
-        if node.nodeName == 'field':
-            if node.firstChild.firstChild.nodeValue == 'expectedResults' and node.childNodes[1].firstChild.nodeName == 'enumerated_list':
-                self.expectedResults = []
-                for list_item in node.childNodes[1].firstChild.childNodes:
-                    self.expectedResults.append(list_item.firstChild.firstChild.nodeValue)
-            elif node.firstChild.firstChild.nodeValue == 'steps' and node.childNodes[1].firstChild.nodeName == 'enumerated_list':
-                self.steps = []
-                for list_item in node.childNodes[1].firstChild.childNodes:
-                    self.steps.append(list_item.firstChild.firstChild.nodeValue)
-            elif node.firstChild.firstChild.nodeValue == 'tags':
-                setattr(self, node.firstChild.firstChild.nodeValue, node.childNodes[1].firstChild.firstChild.nodeValue.split(", "))
-            else:
-                try:
-                    setattr(self, node.firstChild.firstChild.nodeValue, node.childNodes[1].firstChild.firstChild.nodeValue)
-                except:
-                    sys.stderr.write("Problem parsing comment for test: {}".format(self.name))
 
 
 def parse_config(files):
