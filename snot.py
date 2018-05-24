08866082fded77f61ff075ce21fea314b2c4798c
Steve Jensen <steve.jensen@vivint.com>
2018-05-24 10:20:12 -0600

    Try getting the snotDataDrivenFile from the test context rather than the module

--- snot.py
+++ snot.py
@@ -425,7 +425,7 @@ class SlickAsSnotPlugin(nose.plugins.Plugin):
                         elif attribute_name not in ('expectedResults', 'component', 'steps'):
                             result_attributes[attribute_name] = str(attribute_value)
                     if hasattr(test, 'data_driven') and test.data_driven:
-                        method_file = sys.modules[getattr(test.test, testmethod).__module__].__file__
+                        method_file = getattr(test.test, 'context').__file__
                         if method_file.startswith(os.getcwd()):
                             method_file = method_file[len(os.getcwd()) + 1:]
                         if method_file.endswith('pyc'):
